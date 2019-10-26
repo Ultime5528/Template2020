bdb8fc7cc8eeb73d464b4c4716bd2313fb93724a
Ultime 5528 <keranna.first@gmail.com>
2019-10-26 02:46:37 -0400

    Ajout exemple de GroupCommand.

---
+++ src/main/java/frc/robot/commands/ExampleCommandGroup.java
@@ -0,0 +1,27 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+/**
+ * ExampleCommandGroup.
+ * 
+ * Dans cet exemple, la classe parent est SequentialCommandGroup. 
+ * Elle peut être changée, au besoin. Les 4 choix possibles :
+ * - SequentialCommandGroup
+ * - ParallelCommandGroup
+ * - ParallelRaceGroup
+ * - ParallelDeadlineGroup
+ */
+public class ExampleCommandGroup extends SequentialCommandGroup {
+
+    /**
+     * Constructeur.
+     * On doit ajouter, en paramètres, les sous-systèmes utilisés par le groupe.
+     */
+    public ExampleCommandGroup(/* ExampleSubsystem exampleSubsystem, ... */) {
+        // Ajouter les commandes à exécuter dans le groupe.
+        // addCommands(command1, command2, ...);
+        
+    }
+    
+}
